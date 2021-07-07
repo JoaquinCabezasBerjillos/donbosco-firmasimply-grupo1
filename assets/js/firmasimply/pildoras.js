2bccf5b64d4ec124ebac7a0abc8323497d2b5ff5
unknown <joaquincabezasberjillos@gmail.com>
2021-07-07 15:26:37 +0200

    actualizacion

--- assets/js/firmasimply/pildoras.js
+++ assets/js/firmasimply/pildoras.js
@@ -1,5 +1,5 @@
 import Auth from './Modules/Auth/Auth.js';
-import Pildora from './Modules/Pildora.js';
+import Pildoras from './Modules/Pildora.js';
 
 async function getListadoPildoras() {
     let res = await Pildoras.getListadoPildoras();
@@ -24,7 +24,7 @@ async function getListadoPildoras() {
 }
 
 // Pildora Class: Represents a Pildora
-getListadoTareas();
+getListadoPildoras();
 
 class Pildora {
 	constructor(nombre, descripcion, fecha_presentacion) {
@@ -34,131 +34,115 @@ class Pildora {
 	}
 }
 class UI {
-	static displayTareas() {
-		const tareas = Store.getTareas();
-
-		tareas.forEach((tarea) => UI.)
-	}
-
-
-}
-class Store {
-	static addPildora(pildora) {
-        const pildoras = Store.getPildoras();
-        pildoras.push(pildora);
-        localStorage.setItem('pildoras', JSON.stringify(pildoras));
-    }
-	static removePildora(fecha_presentacion) {
-        const pildoras = Store.getPildoras();
-        pildoras.forEach((pildora, index) => {
-            if(pildora.fecha_presentacion === fecha_presentacion){
-                pildoras.splice(index, 1);
-            }
-        });
-        localStorage.setItem('pildoras', JSON.stringify(pildoras));
-    }
-	static getPildoras() {
-        let pildoras = localStorage.getItem('pildoras');
-        if(pildoras === null) {
-            return [];
-        } else {
-            return JSON.parse(pildoras);
-        }
-    }
-}
-
-// UI Class: Handle UI Tasks
-class UI {
-	static showAlert(message, className) {
-		const div = document.createElement('div')
-		div.innerText = message
-		div.className = `alert alert-${className}`
-		document.getElementById('pildora-form').prepend(div)
-
-		setTimeout(() => {
-			div.remove()
-		}, 2000)
-	}
-
-	static deletePildora(target) {
-		if (target.classList.contains('delete')) {
-			// we clicked the X icon
-			target.parentElement.parentElement.remove()
-		}
-	}
-	static clearFields() {
-		const descripcion = document.getElementById('descripcion')
-		const nombre = document.getElementById('nombre')
-		const fecha_presentacion = document.getElementById('fecha_presentacion')
-		descripcion.value = ''
-		nombre.value = ''
-		fecha_presentacion.value = ''
-	}
-
 	static displayPildoras() {
 		const pildoras = Store.getPildoras();
-		pildoras.forEach((pildora) => UI.addPildoraToList(pildora))
+
+		pildoras.forEach((pildora) => UI.addPildoraToList(pildora));
 	}
 
-	static addPildoraToList(pildora) {
-		const list = document.getElementById('pildora-list')
+	static addTareaToList(pildora){
+		const list = document.querySelector('#pildora-list');
 
-		const row = document.createElement('tr')
+		const row = document.createElement('tr');
 
-		row.innerHTML = `
-    <td>${pildora.nombre}</td>
-    <td>${pildora.descripcion}</td>
-    <td>${pildora.fecha_presentacion}</td>
-	<td></td>
-	<td></td>
-    <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-    `
+		row.innerHTML =  `
+        <td>${pildora.nombre}</td>
+        <td>${pildora.categoria_id}</td>
+        <td>${pildora.descripcion}</td>
+        <td></td>
+        <td></td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+      `;
 
-		list.appendChild(row)
+    list.appendChild(row);
+  }
+
+  	static deletePildora(el){
+		if (el.classList.contains('delete')){
+			el.parentElement.parentElement.remove();
+		}
+	}
+
+	static clearFields() {
+		document.querySelector('#nombre').value = '';
+		document.querySelector('#descripcion').value = '';
+		document.querySelector('#fecha_presentacion').value = '';
 	}
 }
 
-// Event: Display Pildoras
-UI.displayPildoras()
-
-// Event: Add a Pildora
-document.querySelector('#pildora-form').addEventListener('submit', addAPildora, false)
-
-function addAPildora(e) {
-	// prevent actual submission
-	e.preventDefault()
-
-	// Get Form Values
-	const descripcion = document.getElementById('descripcion').value
-	const nombre = document.getElementById('nombre').value
-	const fecha_presentacion = document.getElementById('fecha_presentacion').value
-
-	if (!descripcion || !nombre || !fecha_presentacion) {
-		UI.showAlert('Please enter correct details', 'danger')
-		return
+class Store {
+	static getPildoras() {
+	  let pildoras;
+	  if (localStorage.getItem('pildoras') === null) {
+		pildoras = [];
+	  } else {
+		pildoras = JSON.parse(localStorage.getItem('pildoras'));
+	  }
+  
+	  return pildoras;
 	}
+  
+	static addTarea(pildora) {
+	  const pildoras = Store.getPildoras();
+	  pildoras.push(pildora);
+	  localStorage.setItem('pildoras', JSON.stringify(pildoras));
+	}
+  
+	static removeTarea(pildora) {
+	  const pildoras = Store.getTareas();
+  
+	  pildoras.forEach((pildora, index) => {
+		if (pildora.descripcion === descripcion) {
+		  pildora.splice(index, 1);
+		}
+	  });
+  
+	  localStorage.setItem('pildoras', JSON.stringify(pildoras));
+	}
+  }
 
-	// Instantiate a new Pildora object
-	const pildora = new Pildora(nombre, descripcion, fecha_presentacion)
-
-	// Add pildora object to UI
-	UI.addPildoraToList(pildora)
-
-	// Add pildora to store
-	Store.addPildora(pildora)
-
-	UI.showAlert('Pildora Added', 'success')
-
-	// Clear fields
-	UI.clearFields()
-}
-
-document.getElementById('pildora-list').addEventListener('click', handleRemove)
-function handleRemove(e) {
-	// Remove pildora from UI
-	UI.deletePildora(e.target)
-	UI.showAlert('Pildora Removed', 'success')
-
-	// Remove pildora from store
-	Store.removePildora(e.target.parentElement.previousElementSibling.textContent)
-}
+  document.addEventListener('DOMContentLoaded', UI.displayTareas);
+  console.log(1);
+  // Event: Add a Pildora
+  document.querySelector('#pildora-form').addEventListener('submit', (e) => {
+	e.preventDefault();
+  
+  
+	// Get form values
+	const nombre = document.querySelector('#nombre').value;
+	const descripcion = document.querySelector('#descripcion').value;
+	const fecha_presentacion = document.querySelector('#fecha_presentacion').value;
+	Pildoras.crearPildora({ nombre, descripcion, fecha_presentacion, user_id: 2, categoria_id: 1 });
+	// // Validate
+	if(nombre === '' || descripcion === '' || fecha_presentacion === '') {
+	  UI.showAlert('Please fill in all fields', 'danger');
+	} else {
+  
+	  const pildora = new Pildora(nombre, descripcion, fecha_presentacion)
+  
+	  // Add Pildora to UI
+	  UI.addTareaToList(pildora)
+  
+	  // Add Pildora to store
+	 Store.addPildora(pildora);
+  
+	  // Show success message
+	//  UI.showAlert('Pildora Added', 'success');
+	UI.showAlert('Pildora borrada','success')
+  
+	  // Clear fields
+	  UI.clearFields();
+	}
+  });
+  
+  // Event: Remove a Pildora
+  document.querySelector('#pildora-list').addEventListener('click', (e) => {
+	// Remove Pildora from UI
+	UI.deletePildora(e.target);
+  
+	// Remove Pildora from store
+	Store.removePildora(e.target.parentElement.previousElementSibling.textContent);
+  
+	// Show success message
+	// UI.showAlert('Pildora Removed', 'success');
+  });
